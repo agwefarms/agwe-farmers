5471aedc83b1a96350ee3d830a4a3167def66800
AGWE Farmers <76869865+agwefarms@users.noreply.github.com>
2021-01-04 00:26:39 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const features = [
     description: (
       <>
         AGWE Farmers servers are free to use, you&apos;ll just need a copy of 
-        <a href="https://www.minecraft.net/en-us/store/minecraft-java-edition" target="blank" rel="nofollow">Minecraft Java Edition</a> to connect.
+        <a href="https://www.minecraft.net/en-us/store/minecraft-java-edition" target="blank" rel="nofollow"> Minecraft Java Edition</a> to connect.
         We believe in helping those who help our communities thrive.
       </>
     ),
