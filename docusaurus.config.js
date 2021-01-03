8b6205f90d9cc324c8e0f5c93eaee9fc086f1d72
agwefarms <agwefarms@pm.me>
2021-01-03 10:39:27 +0000

    agwe
    Signed-off-by: agwefarms <agwefarms@pm.me>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'AGWE Farmers',
+  tagline: 'AGWE farmers servers',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'agwe', // Usually your GitHub org/user name.
+  projectName: 'agwe', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'AGWE',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'AGWE Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -22,8 +22,8 @@ module.exports = {
         },
         {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          href: 'https://github.com/agwefarms/agwe-farmers',
+          label: 'AGWE Github',
           position: 'right',
         },
       ],
@@ -86,13 +86,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.json'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/DirtyF/docusaurus-forestry-starter/edit/master/website/',
+            'https://github.com/agwefarms/agwe-farmers/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/DirtyF/docusaurus-forestry-starter/edit/master/website/blog/',
+            'https://github.com/agwefarms/agwe-farmers/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
